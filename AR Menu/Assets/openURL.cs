f8d936d52af6f438ef24214a7ca89eef8ed8a65d
prajnavohraa <prajna21345@iiitd.ac.in>
2023-10-11 00:05:11 +0530

    place order button

---
+++ AR Menu/Assets/openURL.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openURL : MonoBehaviour
+{
+    public void openurl(){
+        Application.OpenURL("https://docs.google.com/forms/d/e/1FAIpQLScySgLnbRnh4cpv8m-gpt7JxlHR7v6mqEoTAuMWTnQxYhKSvg/viewform?usp=sf_link");
+    }
+    
+}
