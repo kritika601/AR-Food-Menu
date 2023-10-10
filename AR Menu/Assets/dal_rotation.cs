271c05d636a51116b0accad55d8575308a2a6fe0
prajnavohraa <prajna21345@iiitd.ac.in>
2023-10-10 19:05:53 +0530

    dal rotation

---
+++ AR Menu/Assets/dal_rotation.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dal_rotation : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0,.7f,0);
+    }
+}
